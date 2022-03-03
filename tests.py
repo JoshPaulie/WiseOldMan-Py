0e213c4fcc8de94d82c82af08393354b4db26d35
JoshPaulie <30572280+JoshPaulie@users.noreply.github.com>
2022-03-03 07:17:47 -0600

    Rename tests.py

--- tests.py
+++ tests.py
@@ -1,14 +1,12 @@
-"""Docstrings are dumber"""
 from wiseoldman_py import wiseoldman
-from pprint import pprint
 
 wom = wiseoldman.WiseOldMan()
 bexlii = wom.get_player("bexlii")
 
-# Tests
+
 def test_get_player():
     assert isinstance(bexlii.player_data, dict)
 
 
-# Debugging
-print(bexlii.skill.attack_ehp)
+def test_player_skill():
+    assert isinstance(bexlii.skill.attack_experience, int)
