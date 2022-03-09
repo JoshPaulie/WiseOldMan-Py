c7c7a5701af753721462c46c41cabce69ea6ec00
JoshPaulie <30572280+JoshPaulie@users.noreply.github.com>
2022-03-09 06:52:11 -0600

    Fix Lvl 99 bug

--- wiseoldman_py/modules/calculators.py
+++ wiseoldman_py/modules/calculators.py
@@ -1,7 +1,13 @@
 from .static import LEVEL_EXPERIENCE_MILESTONES
 
 
-def get_level(xp: int) -> int:  # type: ignore
+def get_level(xp: int) -> int:
+    """Calculates a skill's lever up to 99"""
+    if xp >= 13034431:
+        return 99
+
     for indx, level_milestone in enumerate(LEVEL_EXPERIENCE_MILESTONES):
         if xp >= level_milestone and xp < LEVEL_EXPERIENCE_MILESTONES[indx + 1]:
             return indx + 1
+
+    return 0
