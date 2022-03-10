cee9a78e82e2e74453b9c43bb4dd764273d9f213
JoshPaulie <joshpaullee@gmail.com>
2022-03-09 19:40:49 -0600

    Allow player lookup by username or id

--- tests.py
+++ tests.py
@@ -2,12 +2,21 @@ from wiseoldman_py import wiseoldman
 from wiseoldman_py.player import Player
 
 wom = wiseoldman.WiseOldMan()
-bexlii = wom.get_player("bexlii")
 
 
-def test_get_player():
+def test_get_player_by_username():
+    """Test for player by username"""
+    bexlii = wom.get_player(osrs_username="bexlii")
     assert isinstance(bexlii, Player)
 
 
-def test_player_skill():
+def test_get_player_by_user_id():
+    """Test for player by user id"""
+    bexlii = wom.get_player(osrs_user_id=369451)
+    assert isinstance(bexlii, Player)
+
+
+def test_player_skill_level():
+    """Test for player's skill, which is a custom property that should be checked."""
+    bexlii = wom.get_player(osrs_username="bexlii")
     assert isinstance(bexlii.latest_stats.attack.level, int)
--- wiseoldman_py/wiseoldman.py
+++ wiseoldman_py/wiseoldman.py
@@ -4,10 +4,20 @@ from .player import Player
 
 
 class WiseOldMan:
+    """Core WiseOldMan-Py"""
+
     def __init__(self) -> None:
         self.base_url = "https://api.wiseoldman.net"
 
-    def get_player(self, osrs_username: str) -> Player:
+    def get_player(self, *, osrs_username: str | None = None, osrs_user_id: int | None = None) -> Player:
         """Searches WOM for player, returns Player with latest snapshot"""
-        player_json = requests.get(f"{self.base_url}/players/username/{osrs_username}").json()
+        if osrs_username is None and osrs_user_id is None:
+            raise ValueError("Either a username or user id must be provided")
+
+        player_json = {}
+        if osrs_username:
+            player_json = requests.get(f"{self.base_url}/players/username/{osrs_username}").json()
+        if osrs_user_id:
+            player_json = requests.get(f"{self.base_url}/players/{osrs_user_id}").json()
+
         return Player(**player_json)
