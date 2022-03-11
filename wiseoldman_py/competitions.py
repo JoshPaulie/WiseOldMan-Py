c7fe9fa218efa809aca538193b89fb646cd028e3
JoshPaulie <30572280+JoshPaulie@users.noreply.github.com>
2022-03-11 15:17:07 -0600

    Created Competition and Participation classes

---
+++ wiseoldman_py/competitions.py
@@ -0,0 +1,26 @@
+import datetime
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class Competition(BaseModel):
+    competition_id: int = Field(alias="id")  # ? 'id' is a keyword
+    title: str
+    metric: str
+    score: int
+    starts_at: Optional[datetime.datetime] = Field(alias="startsAt")
+    ends_at: Optional[datetime.datetime] = Field(alias="endsAt")
+    competition_type: str = Field(alias="type")  # ? 'type' is a keyword
+    group_id: int
+    created_at: Optional[datetime.datetime] = Field(alias="createdAt")
+    updated_at: Optional[datetime.datetime] = Field(alias="updatedAt")
+    duration: str  # ? You'd think this would be an int, it's not
+    participant_count: int = Field(alias="participantCount")
+
+
+class Participation(BaseModel):
+    player_id: int = Field(alias="playerId")
+    competition_id: int = Field(alias="competitionId")
+    start_snapshot_id: int = Field(alias="startSnapshotId")
+    end_snapshot_id: int = Field(alias="endSnapshotId")
