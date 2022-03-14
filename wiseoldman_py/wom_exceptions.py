0b0e532ca140e633feef773bb6ed01571a378a04
JoshPaulie <30572280+JoshPaulie@users.noreply.github.com>
2022-03-14 16:06:20 -0500

    Removed PlayerHasNoAchievementsError

--- wiseoldman_py/wom_exceptions.py
+++ wiseoldman_py/wom_exceptions.py
@@ -1,7 +1,2 @@
 class PlayerNotFoundError(ValueError):
     pass
-
-
-# ? I wasn't sure what to inherit, docs lead me to believe this is fine
-class PlayerHasNoAchievementsError(Exception):
-    pass
