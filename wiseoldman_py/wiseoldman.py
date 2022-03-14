c29c27a9b4262cf9a289df81c53878cdff682464
JoshPaulie <30572280+JoshPaulie@users.noreply.github.com>
2022-03-13 20:20:29 -0500

    Added check for player not found (in both get_player)

--- wiseoldman_py/wiseoldman.py
+++ wiseoldman_py/wiseoldman.py
@@ -1,5 +1,8 @@
 import requests
 
+from wiseoldman_py.wom_exceptions import PlayerNotFoundError
+
+from .groups import Group
 from .player import Achievement, Player
 
 
@@ -20,6 +23,11 @@ class WiseOldMan:
         if user_id:
             player_json = requests.get(f"{self.base_url}/players/{user_id}").json()
 
+        if player_json.get("message", None):
+            message = player_json["message"]
+            if message == "Player not found.":
+                raise PlayerNotFoundError(f"Player not found.")
+
         return Player(**player_json)
 
     def get_player_achievements(
@@ -29,15 +37,32 @@ class WiseOldMan:
         if username is None and user_id is None:
             raise ValueError("Either a username or user id must be provided")
 
-        player_json = {}
+        player_achievement_json = {}
         if username:
-            player_json = requests.get(f"{self.base_url}/players/username/{username}/achievements").json()
+            player_achievement_json = requests.get(
+                f"{self.base_url}/players/username/{username}/achievements"
+            ).json()
         if user_id:
-            player_json = requests.get(f"{self.base_url}/players/{user_id}/achievements").json()
+            player_achievement_json = requests.get(f"{self.base_url}/players/{user_id}/achievements").json()
+
+        if isinstance(player_achievement_json, dict):
+            if player_achievement_json.get("message", None):
+                message = player_achievement_json["message"]
+                if message == "Player not found.":
+                    raise PlayerNotFoundError(f"Player not found.")
 
         # ? No comp to make sure an empty list can be returned
         achievements = []
-        for a in player_json:
+        for a in player_achievement_json:
             achievements.append(Achievement(**a))
 
         return achievements
+
+    def get_group(self, *, group_id: int | None) -> Group:
+        """Searches WOM for player, returns Player with latest snapshot"""
+        if group_id is None:
+            raise ValueError("A group id must be provided")
+
+        group_json = requests.get(f"{self.base_url}/groups/{id}").json()
+
+        return Group(**group_json)
