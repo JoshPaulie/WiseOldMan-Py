a0681123ec183fd6a1808e0cb343fa3f932030d7
JoshPaulie <30572280+JoshPaulie@users.noreply.github.com>
2022-03-14 15:51:03 -0500

    Made group_id optional

--- wiseoldman_py/competitions.py
+++ wiseoldman_py/competitions.py
@@ -12,7 +12,7 @@ class Competition(BaseModel):
     starts_at: Optional[datetime.datetime] = Field(alias="startsAt")
     ends_at: Optional[datetime.datetime] = Field(alias="endsAt")
     competition_type: str = Field(alias="type")  # ? 'type' is a keyword
-    group_id: int
+    group_id: Optional[int]
     created_at: Optional[datetime.datetime] = Field(alias="createdAt")
     updated_at: Optional[datetime.datetime] = Field(alias="updatedAt")
     duration: str  # ? You'd think this would be an int, it's not
